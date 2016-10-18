27a6c4f3a3c5802fa0ad6c981012f4ca16b050ab
jachan1 <jachan1@gmail.com>
2016-10-18 14:37:24 -0400

    added rough tests

--- tab1.R
+++ tab1.R
@@ -24,27 +24,52 @@ tab1_fxn_hpr <- function(ds, tab_in, pp, mp){
   }
 }
 
-tab1_fxn <- function(tab_in, ds, grp, pp=1, mp=1){
+test_grp <- function(ds, grp, tab_in, sub="referenceid"){
+  fm1 <- formula(sprintf("%s ~ %s", tab_in$var, grp))
+  if(tab_in$type == "c"){
+    lm1 <- lm(fm1, data=na.omit(ds[c(tab_in$var, grp, sub)]))
+    slm1 <- summary(lm1)
+    anova(lm1)[1, "Pr(>F)"]
+  } else {
+    chisq.test(ds[[tab_in$var]], ds[[grp]])$p.value
+  }
+}
+
+tab1_fxn <- function(tab_in, ds, grp, pp=1, mp=1, test=F){
   # print(tab_in)
   # if(tab_in$var=="ddx") browser()
-  ## tab_in should include columns "varnm", "var", "type", "group"
+  ## tab_in should include columns "varnm", "var", "type", *optional* "group"
   ## if type == "b" then target should be available
   # if(tab_in$var == "white") browser()
+  if(!"group" %in% names(tab_in)) tab_in$group = ""
   if(!missing(grp)){
-    ds %>% group_by_(grp) %>% do(tab1_fxn_hpr(.,tab_in, pp=pp, mp=mp))
+    ds_out <- ds %>% group_by_(grp) %>% do(tab1_fxn_hpr(.,tab_in, pp=pp, mp=mp))
+    if(test){
+      p <- tryCatch(test_grp(ds, grp, tab_in), error=function(e) NA)
+      ds_out <- ds_out %>% ungroup %>% mutate(p=p)
+    }
+    ds_out
   } else {
     ds %>% do(tab1_fxn_hpr(., tab_in, pp=pp, mp=mp))
   }
 }
 
-grp_tirc <- function(x, rgroup_col="group", grp="study_grp", rnames="Characteristic"){
+grp_tirc <- function(x, rgroup_col="group", grp="study_grp", rnames="Characteristic", p=F){
+  if(p == F) p <- as.character()
   if(!rgroup_col %in% names(x)) x[[rgroup_col]]=""
-  cols <- setdiff(names(x), grp)
+  cols <- setdiff(names(x), c(grp, p))
   # print(cols)
   wide <- Reduce(function(x, y) merge(x, y, by=c(rgroup_col, rnames), all=T), lapply(unique(x[[grp]]), function(y) x[x[[grp]]==y, cols]))
-  grps <- unique(x[[grp]])
-  names(wide) <- c(rgroup_col, rnames, rep(setdiff(cols, c(rnames, rgroup_col)), length(grps)))
-  TIRC(wide, rnames=rnames, rgroup_col=rgroup_col, cgroup=grps, n.cgroup=rep(length(cols)-2, length(grps)))
+  if(length(p) > 0) wide <- wide %>% left_join(unique(x[c(rnames, p)]), by=rnames)
+  names(wide) <- c(rgroup_col, rnames, rep(setdiff(cols, c(rnames, rgroup_col)), length(unique(x[[grp]]))), p)
+  grps <- as.character(unique(x[[grp]]))
+  ngrps <- rep(length(cols)-2, length(grps))
+  if(length(p) > 0) {
+    grps <- c(grps, "")
+    ngrps <- c(ngrps, 1)
+    wide[[p]] <- round(wide[[p]], 3)
+  }
+  TIRC(wide, rnames=rnames, rgroup_col=rgroup_col, cgroup=grps, n.cgroup=ngrps)
 }
 
 # tab1 <- bind_rows(data_frame(varnm='Age (yrs)', var='age', group='', type='c'),
